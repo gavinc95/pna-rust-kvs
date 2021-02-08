67899dc8cd1c5e84645e762e62e2e99f12195fee
Gavin Chan <gavinwhchan@gmail.com>
2021-02-08 09:56:59 -0800

    add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,6 @@
-use std::collections::HashMap;
+#![deny(missing_docs)]
+//! A simple in-memory key-value store.
 
-pub struct KvStore {
-    store: HashMap<String, String>,
-}
+pub use kv::KvStore;
 
-impl KvStore {
-    pub fn new() -> KvStore {
-        KvStore {
-            store: HashMap::new(),
-        }
-    }
-
-    pub fn set(&mut self, key: String, value: String) {
-        self.store.insert(key, value);
-    }
-
-    pub fn get(&self, key: String) -> Option<String> {
-        match self.store.get(&key) {
-            Some(value) => Some(value.to_string()),
-            None => None,
-        }
-    }
-
-    pub fn remove(&mut self, key: String) {
-        self.store.remove(&key);
-    }
-}
+mod kv;
